fa3598e43427c2e623564a01ad294e18584b2dd4
Paweł <robakcode@gmail.com>
2024-03-02 10:29:32 +0100

    default measure PoC

--- src/elements/architecture/buildings/basic/home/home.model.ts
+++ src/elements/architecture/buildings/basic/home/home.model.ts
@@ -1,5 +1,9 @@
 import * as THREE from "three";
 
+import { defaultMeasure } from "../../../../../settings/defaultMeasure";
+
+const dm = (value: number) => defaultMeasure * value;
+
 const homeGroup = new THREE.Group();
 
 const defaultMaterial = new THREE.MeshMatcapMaterial({
@@ -13,46 +17,46 @@ const defaultMaterial = new THREE.MeshMatcapMaterial({
 /**
  * Front and back walls
  */
-const frontWallGeometry = new THREE.BoxGeometry(8, 10, 0.025);
+const frontWallGeometry = new THREE.BoxGeometry(dm(8), dm(10), dm(0.025));
 const frontWallModel = new THREE.Mesh(frontWallGeometry, defaultMaterial);
 frontWallModel.position.set(0, 0, 0);
 homeGroup.add(frontWallModel);
 
 const backWallModel = new THREE.Mesh(frontWallGeometry, defaultMaterial);
-backWallModel.position.set(0, 0, -8);
+backWallModel.position.set(0, 0, dm(-8));
 homeGroup.add(backWallModel);
 
 /**
  * Side walls
  */
-const sideWallGeometry = new THREE.BoxGeometry(0.025, 10, 8);
+const sideWallGeometry = new THREE.BoxGeometry(dm(0.025), dm(10), dm(8));
 const leftSideWallModel = new THREE.Mesh(sideWallGeometry, defaultMaterial);
-leftSideWallModel.position.set(-4, 0, -4);
+leftSideWallModel.position.set(dm(-4), 0, dm(-4));
 homeGroup.add(leftSideWallModel);
 
 const rightSideWallModel = new THREE.Mesh(sideWallGeometry, defaultMaterial);
-rightSideWallModel.position.set(4, 0, -4);
+rightSideWallModel.position.set(dm(4), 0, dm(-4));
 homeGroup.add(rightSideWallModel);
 
 /**
  * Roof and floor
  */
-const floorGeometry = new THREE.BoxGeometry(8, 0.025, 8);
+const floorGeometry = new THREE.BoxGeometry(dm(8), dm(0.025), dm(8));
 const floorModel = new THREE.Mesh(floorGeometry, defaultMaterial);
-floorModel.position.set(0, -5, -4);
+floorModel.position.set(0, dm(-5), dm(-4));
 homeGroup.add(floorModel);
 
-const roofGeometry = new THREE.BoxGeometry(8, 0.025, 8);
+const roofGeometry = new THREE.BoxGeometry(dm(8), dm(0.025), dm(8));
 const roofModel = new THREE.Mesh(roofGeometry, defaultMaterial);
-roofModel.position.set(0, 5, -4);
+roofModel.position.set(0, dm(5), dm(-4));
 homeGroup.add(roofModel);
 
 /**
  * Roof cone
  */
-const roofConeGeometry = new THREE.ConeGeometry(6.8, 2, 4);
+const roofConeGeometry = new THREE.ConeGeometry(dm(6.8), dm(2), 4);
 const roofConeModel = new THREE.Mesh(roofConeGeometry, defaultMaterial);
-roofConeModel.position.set(0, 6, -4);
+roofConeModel.position.set(0, dm(6), dm(-4));
 roofConeModel.rotation.y = Math.PI / 4;
 homeGroup.add(roofConeModel);
 
@@ -61,26 +65,26 @@ homeGroup.add(roofConeModel);
  * right top and left top corner are rounded
  */
 const doorFrame = new THREE.Shape();
-doorFrame.moveTo(-1, 1.5);
-doorFrame.lineTo(-1, -2);
-doorFrame.lineTo(1, -2);
-doorFrame.lineTo(1, 1.5);
-doorFrame.quadraticCurveTo(1, 2, 0.5, 2);
-doorFrame.lineTo(-0.5, 2);
-doorFrame.quadraticCurveTo(-1, 2, -1, 1.5);
+doorFrame.moveTo(dm(-1), dm(1.5));
+doorFrame.lineTo(dm(-1), dm(-2));
+doorFrame.lineTo(dm(1), dm(-2));
+doorFrame.lineTo(dm(1), dm(1.5));
+doorFrame.quadraticCurveTo(dm(1), dm(2), dm(0.5), dm(2));
+doorFrame.lineTo(dm(-0.5), dm(2));
+doorFrame.quadraticCurveTo(dm(-1), dm(2), dm(-1), dm(1.5));
 
 const doorFrameHole = new THREE.Path();
-doorFrameHole.moveTo(-0.7, -2);
-doorFrameHole.lineTo(-0.7, 1.4);
-doorFrameHole.quadraticCurveTo(-0.7, 1.7, -0.5, 1.7);
-doorFrameHole.lineTo(0.5, 1.7);
-doorFrameHole.quadraticCurveTo(0.7, 1.7, 0.7, 1.4);
-doorFrameHole.lineTo(0.7, -2);
+doorFrameHole.moveTo(dm(-0.7), dm(-2));
+doorFrameHole.lineTo(dm(-0.7), dm(1.4));
+doorFrameHole.quadraticCurveTo(dm(-0.7), dm(1.7), dm(-0.5), dm(1.7));
+doorFrameHole.lineTo(dm(0.5), dm(1.7));
+doorFrameHole.quadraticCurveTo(dm(0.7), dm(1.7), dm(0.7), dm(1.4));
+doorFrameHole.lineTo(dm(0.7), dm(-2));
 doorFrame.holes.push(doorFrameHole);
 
 const doorFrameExtrudeSettings = {
-  steps: 2,
-  depth: 0.025,
+  steps: dm(2),
+  depth: dm(0.025),
   bevelEnabled: false,
 };
 
@@ -96,23 +100,23 @@ const doorFrameMaterial = new THREE.MeshMatcapMaterial({
   depthTest: true,
 });
 const doorFrameModel = new THREE.Mesh(doorFrameGeometry, doorFrameMaterial);
-doorFrameModel.position.set(0, -3, 0.025);
+doorFrameModel.position.set(0, dm(-3), dm(0.025));
 homeGroup.add(doorFrameModel);
 
 /**
  * Door
  */
 const door = new THREE.Shape();
-door.moveTo(-0.7, -2);
-door.lineTo(-0.7, 1.4);
-door.quadraticCurveTo(-0.7, 1.7, -0.5, 1.7);
-door.lineTo(0.5, 1.7);
-door.quadraticCurveTo(0.7, 1.7, 0.7, 1.4);
-door.lineTo(0.7, -2);
+door.moveTo(dm(-0.7), dm(-2));
+door.lineTo(dm(-0.7), dm(1.4));
+door.quadraticCurveTo(dm(-0.7), dm(1.7), dm(-0.5), dm(1.7));
+door.lineTo(dm(0.5), dm(1.7));
+door.quadraticCurveTo(dm(0.7), dm(1.7), dm(0.7), dm(1.4));
+door.lineTo(dm(0.7), dm(-2));
 
 const doorExtrudeSettings = {
-  steps: 2,
-  depth: 0.05,
+  steps: dm(2),
+  depth: dm(0.05),
   bevelEnabled: false,
 };
 
@@ -125,13 +129,13 @@ const doorMaterial = new THREE.MeshMatcapMaterial({
   depthTest: true,
 });
 const doorModel = new THREE.Mesh(doorGeometry, doorMaterial);
-doorModel.position.set(0, -3, 0.025);
+doorModel.position.set(0, dm(-3), dm(0.025));
 homeGroup.add(doorModel);
 
 /**
  * Windows
  */
-const windowGeometry = new THREE.BoxGeometry(2, 2, 0.025);
+const windowGeometry = new THREE.BoxGeometry(dm(2), dm(2), dm(0.025));
 const windowMaterial = new THREE.MeshMatcapMaterial({
   color: "lightblue",
   transparent: true,
@@ -140,11 +144,11 @@ const windowMaterial = new THREE.MeshMatcapMaterial({
   depthTest: true,
 });
 const leftWindowModel = new THREE.Mesh(windowGeometry, windowMaterial);
-leftWindowModel.position.set(-2.5, 2.5, 0.025);
+leftWindowModel.position.set(dm(-2.5), dm(2.5), dm(0.025));
 homeGroup.add(leftWindowModel);
 
 const rightWindowModel = new THREE.Mesh(windowGeometry, windowMaterial);
-rightWindowModel.position.set(2.5, 2.5, 0.025);
+rightWindowModel.position.set(dm(2.5), dm(2.5), dm(0.025));
 homeGroup.add(rightWindowModel);
 
 export const homeModel = homeGroup;
